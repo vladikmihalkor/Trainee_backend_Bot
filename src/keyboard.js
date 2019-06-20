5e233f73eb3bb2fa0ab65e1d11e7f6b6b5a59049
vladikmihalkor <52046079+vladikmihalkor@users.noreply.github.com>
2019-06-20 20:33:01 +0300

    Update keyboard.js

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,7 +1,6 @@
 const kb = require('./keyboard-buttons')
 module.exports = {
-
 	Keyboard:[
-        [kb.Keyboard.One,kb.Keyboard.Four,kb.Keyboard.Five,kb.Keyboard.Nine,kb.Keyboard.Ten]
+        	[kb.Keyboard.One,kb.Keyboard.Four,kb.Keyboard.Five,kb.Keyboard.Nine,kb.Keyboard.Ten]
 	]
 }
