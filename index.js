56399da5e2924a8b1df8dda7e5a066dd1f0f8f43
vladikmihalkor <52046079+vladikmihalkor@users.noreply.github.com>
2019-06-20 20:31:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,33 +14,32 @@ bot.on('message', msg => {
     var chatId = msg.chat.id;
     var text = msg.text;
 
-    switch(text) {
-    	case '/start':
-		    bot.sendMessage(chatId, `✋ Привет <b>${msg.from.first_name}</b>.\nЯ бот-конвертер римских и арабских чисел. Напиши мне любое число, систему счисления я сам определю. Значение должно быть в диапазоне [1 .. 3999].\n<i>- Для удобства основные римские числа я вывел в меню.</i>`, {
-		        parse_mode: 'html',
-		        reply_markup: {
-		            resize_keyboard: true,
-		            keyboard: keyboard.Keyboard
-		        }
-		    });
-    		break;
-    	default :
-		    if (helper.isNumeric(text)) {
-		    	if (parseFloat(text) > 3999) {
-				    bot.sendMessage(chatId,`❗️ Значение должно быть в диапазоне [1 .. 3999]`)
-		    	}else{
-				    bot.sendMessage(chatId,`Преобразование десятичного числа в число, записанное римскими цифрами:\n➡️ <b>${text}</b> => <b>${helper.to_roman(text,font_arab,font_roman)}</b>`,{
-				    	parse_mode: 'html'
-		    		});
-		    	}
-		    }else if(helper.isRoman(text)){
-			    bot.sendMessage(chatId,`Преобразование числа, записанного римскими цифрами в десятичное число:\n➡️ <b>${text}</b> => <b>${helper.to_arab(text,font_arab,font_roman)}</b>`,{
-			    	parse_mode: 'html'
-			    });
-		    }else{
-			    bot.sendMessage(chatId,`👌 Прости, но не понимаю тебя.`);
-		    }
-    		break;
-    }	
+    switch (text) {
+        case '/start':
+            bot.sendMessage(chatId, `✋ Привет <b>${msg.from.first_name}</b>.\nЯ бот-конвертер римских и арабских чисел. Напиши мне любое число, систему счисления я сам определю. Значение должно быть в диапазоне [1 .. 3999].\n<i>- Для удобства основные римские числа я вывел в меню.</i>`, {
+                parse_mode: 'html',
+                reply_markup: {
+                    resize_keyboard: true,
+                    keyboard: keyboard.Keyboard
+                }
+            });
+            break;
+        default:
+            if (helper.isNumeric(text)) {
+                if (parseFloat(text) > 3999) {
+                    bot.sendMessage(chatId, `❗️ Значение должно быть в диапазоне [1 .. 3999]`)
+                } else {
+                    bot.sendMessage(chatId, `Преобразование десятичного числа в число, записанное римскими цифрами:\n➡️ <b>${text}</b> => <b>${helper.to_roman(text,font_arab,font_roman)}</b>`, {
+                        parse_mode: 'html'
+                    });
+                }
+            } else if (helper.isRoman(text)) {
+                bot.sendMessage(chatId, `Преобразование числа, записанного римскими цифрами в десятичное число:\n➡️ <b>${text}</b> => <b>${helper.to_arab(text,font_arab,font_roman)}</b>`, {
+                    parse_mode: 'html'
+                });
+            } else {
+                bot.sendMessage(chatId, `👌 Прости, но не понимаю тебя.`);
+            }
+            break;
+    }
 });
-
